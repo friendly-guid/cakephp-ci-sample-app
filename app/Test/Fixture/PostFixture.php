977f4fa8d9d173db352ef29e6ecf99c8f36b8116
friendly-guid <japan.moc.liamg@gmail.com>
2016-12-18 08:31:51 +0000

    change for create test data with fabricate

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -12,19 +12,4 @@ class PostFixture extends CakeTestFixture {
  */
 	public $import = array('model' => 'Post');
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
-			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2016-12-18 06:56:08',
-			'modified' => '2016-12-18 06:56:08'
-		),
-	);
-
 }
