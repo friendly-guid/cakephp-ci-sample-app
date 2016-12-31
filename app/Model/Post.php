324ecf49939da181bb5e51d0dd3b2302bcb20a25
vagrant <vagrant@develop>
2016-12-31 16:25:16 +0000

    add DocBlock comment

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,8 +1,31 @@
 <?php
 App::uses('AppModel', 'Model');
+
+/**
+ * Post Model
+ *
+ * ブログ記事用モデル
+ *
+ * @copyright php_ci_book
+ * @link https://github.com/friendly-guid/cakephp-ci-sample-app/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @auther T.U.
+ *
+ */
 class Post extends AppModel {
+
+    /**
+     * 一覧表示時のタイトルに使用するカラム名
+     *
+     * @var string
+     */
 	public $displayField = 'title';
-	
+
+    /**
+     * バリデーションルール
+     *
+     * @var array
+     */
 	public $validate = [
 		'title' => [
 			'notEmpty' => [
